af3dac9b8c1d389e4ebe5f039e608115d1ff7736
Justin Eldridge <eldridgejm@gmail.com>
2020-06-23 12:29:36 -0700

    Revert "Include Reathoring, with modified quiz extension."
    This reverts commit 54245a819048b29fd141d373bc84d19840b5d17f.

--- extensions/reauthoring/Sequences/Sample/index.rst
+++
@@ -1,9 +0,0 @@
-Exercises in the Sequence
-=========================
-
-.. toctree::
-   :numbered:
-   :glob:
-
-   *_c
-
--- extensions/reauthoring/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='Reauthoring',
-    version='1.0',
-    description='Reauthoring',
-    author='Abelardo Pardo',
-    py_modules=['Sphinx_ext']
-)
